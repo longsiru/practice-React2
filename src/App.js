d4ae9e5553c1f8e5b0c67f9d41e4444ad1b8b2d7
longsiru <sirulong89@gmail.com>
2023-04-06 17:31:12 +0900

    nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div>hello</div>;
+  //localhost:3002/favorites
+  //my-page.com/
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />}></Route>
+        <Route path="/new-meetup" element={<NewMeetupPage />}></Route>
+        <Route path="/favorites" element={<FavoritesPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
