d3c75e5dce7330b35a75884b19d8a5f1f9fe7e9a
longsiru <sirulong89@gmail.com>
2023-04-07 21:29:12 +0900

    usecontext

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,9 @@
 import { useContext } from "react";
+//useContext(): 用于在组件之间共享状态，而不必显式地通过组件树的逐层传递 props。
+//实现步骤：
+//使用createContext创建Context对象
+//在顶层组件通过provider提供数据
+//在底层组件通过useContext函数获取数据
 import FavoritesContext from "../store/favorites-context";
 import MeetupList from "../components/meetups/MeetupList";
 
